9cce96a1f89cda9b11432deb4d89af92a64c6ce9
Pedro Silva <pedro.silva@eokoe.com>
2017-09-01 13:28:27 -0300

    Fix fabric key environment

---
+++ app/src/main/java/com/eokoe/sagui/extensions/Context.kt
@@ -0,0 +1,21 @@
+package com.eokoe.sagui.extensions
+
+import android.content.Context
+import android.content.pm.PackageManager
+import com.eokoe.sagui.utils.LogUtil
+
+/**
+ * @author Pedro Silva
+ * @since 01/09/17
+ */
+fun Context.getManifestValue(key: String): String? {
+    try {
+        val ai = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
+        return ai.metaData.getString(key)
+    } catch (e: PackageManager.NameNotFoundException) {
+        LogUtil.error(this, e)
+    } catch (e: NullPointerException) {
+        LogUtil.error(this, e)
+    }
+    return null
+}
